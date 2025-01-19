25d66c9f6c5f36dd6e22c1364959acdb93f2ab17
Toby Qin <toby.qin@live.com>
2025-01-19 19:37:10 +0800

    remove unused files

--- app/core/deps.py
+++
@@ -1,5 +0,0 @@
-from typing import Generator
-from fastapi import Depends, HTTPException, status
-from motor.motor_asyncio import AsyncIOMotorDatabase
-from app.core.mongodb import get_database
-from app.services.user_service import UserService 
